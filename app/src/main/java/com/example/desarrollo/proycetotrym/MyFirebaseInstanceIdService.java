5258ee5c6dcc359cca8aeefd8d2cfbf2fbce58c1
Reynaldo <reynaldo119@gmail.com>
2019-01-17 10:37:19 -0600

    Segundo

--- app/src/main/java/com/example/desarrollo/proycetotrym/MyFirebaseInstanceIdService.java
+++ app/src/main/java/com/example/desarrollo/proycetotrym/MyFirebaseInstanceIdService.java
@@ -1,4 +1,4 @@
-
+package com.example.desarrollo.proycetotrym;
 
 import android.util.Log;
 
--- app/src/main/java/com/example/desarrollo/proycetotrym/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/desarrollo/proycetotrym/MyFirebaseMessagingService.java
@@ -5,6 +5,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
@@ -54,5 +55,5 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
         notificationManager.notify(0,notificationBuilder.build());
     }
-
+        
 }
